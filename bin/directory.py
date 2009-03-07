0ebe77beba3727e4c55bfabf0bb256448029637c
Tom Parker <palfrey@tevp.net>
2009-03-07 17:33:11 +0000

    Add image viewer

---
+++ bin/directory.py
@@ -0,0 +1,184 @@
+from zipfile import ZipFile
+from rarfile import RarFile
+import os,re
+from types import *
+from StringIO import StringIO
+
+class TreeRar(RarFile):
+	def rar_dirs(self,item):
+		if not hasattr(item,"filename"):
+			print "no filename",item
+			return
+		fname = item.filename
+		print "item",fname
+		if not hasattr(self,"_tree"):
+			self._tree = {}
+		bits = fname.split("\\")
+		where = self._tree
+		for b in bits[:-1]:
+			if b not in where:
+				where[b] = {}
+			where = where[b]
+		where[bits[-1]] = fname
+		#print "tree",self._tree
+
+	def __init__(self,fname):
+		#RarFile.__init__(self,fname,info_callback=self.rar_dirs)
+		RarFile.__init__(self,fname)
+
+class ZipDir:
+	splitter = re.compile(r"[/\\]")
+	def __init__(self,parent,fname):
+		self.parent = parent
+		self.file = fname
+		if fname.find("cbr")!=-1 or fname.find("rar")!=-1:
+			self.zipfile = TreeRar(self.file)
+			print "doing rar",self.file
+			self.israr = True
+		else:
+			self.zipfile = ZipFile(self.file)
+			self.israr = False
+		list = self.zipfile.infolist()
+		self.dir = {}
+		for f in list:
+			path = ZipDir.splitter.split(f.filename)
+			print "f.file",f.filename,path
+			root = self.dir
+			while len(path)>1:
+				if not root.has_key(path[0]) or root[path[0]] == "":
+					root[path[0]] = {}
+				root = root[path[0]]
+				path = path[1:]
+			if path[0]!="" and (path[0] not in root or root[path[0]]=={}):
+				try:
+					root[path[0]] = ""
+				except TypeError:
+					print "dir",self.dir
+					print "root",root,path
+					raise
+		
+		self.path = []
+		self.curdir = self.dir
+		print "self.curdir",self.curdir
+
+	def listdir(self):
+		k = self.curdir.keys()
+		k.sort()
+		return k
+
+	def chdir(self,path):
+		ret = self._chdir(path,self.path,self.curdir)
+		if len(ret)==0:
+			return False
+		(self.path,self.curdir) = ret
+		#assert type(self.curdir) == DictType,(self.path,self.curdir)
+		return True
+	
+	def _chdir(self,path,oldpath,oldcurdir):
+		segments = filter(lambda x:x!='',ZipDir.splitter.split(path))
+		#print "segments",segments,path,self.path
+		for s in segments:
+			if s=="..":
+				if len(oldpath)==0:
+					return []
+				else:
+					oldpath = oldpath[:-1]
+					oldcurdir = self.dir
+					for p in oldpath:
+						oldcurdir = oldcurdir[p]
+					print "ocd,op",oldcurdir,oldpath
+					if len(oldcurdir)==1 and len(oldpath)==0:
+						print "blank root!"
+						self.parent.chdir("..")
+						return []
+					
+			else:
+				if not oldcurdir.has_key(s):
+					return []
+				else:
+					oldpath += [s]
+					oldcurdir = oldcurdir[s]
+		return [oldpath,oldcurdir]
+	
+	def isfile(self,filename):
+		ret = self._chdir(os.path.dirname(filename),self.path,self.curdir)
+		#print "Zip: change dir",os.path.dirname(filename),os.path.basename(filename),ret,self.path
+		if len(ret)==0:
+			return False
+		(newpath,newcurdir) = ret
+		if newcurdir.has_key(os.path.basename(filename)):
+			ret = type(newcurdir[os.path.basename(filename)])
+			print "typing",filename,ret
+			return ret!= DictType
+		else:
+			return False
+	
+	def open(self,filename):
+		f = "/".join(self.path+[filename])
+		return StringIO(self.zipfile.read(f))
+		
+class Directory:
+	#sep = re.compile(r"((?:[A-Za-z]:)|(?:[A-Za-z0-9\-_ ,\.()\[\]]+)|#)|[/\\]")
+	sep = re.compile(r"((?:[A-Za-z]:)|(?:[#A-Za-z0-9\-_ ,!\'\.~()\[\]]+))|[/\\]")
+
+	def __init__(self,path):
+		self.path = path
+		self.dir = None
+		self.subdir = None
+		self.chdir(path)
+	
+	def listdir(self):
+		if self.subdir!=None:
+			return self.subdir.listdir()
+		
+		if self.dir == None:
+			self.dir = os.listdir(self.path)
+			self.dir.sort()
+		return self.dir
+	
+	def isfile(self,filename):
+		if self.subdir!=None:
+			return self.subdir.isfile(filename)
+		else:
+			return os.path.isfile(os.path.join(self.path,filename))
+	
+	compfiles = {"cbz":ZipDir,"zip":ZipDir,"cbr":ZipDir, "rar":ZipDir}
+	
+	def chdir(self,path):
+		print "chdir path",path
+		if (os.path.splitdrive(self.path)[0]=='' and path[0]==os.sep) or (path[0].lower()>='a' and path[0].lower()<='z' and len(path)>1 and path[1] == ':'):
+			self.path = ""
+		
+		sections = filter(lambda x:x!='',Directory.sep.findall(self.path)+Directory.sep.findall(path))
+		print "sections",sections,self.path,path,Directory.sep.findall(path)
+		for x in range(1,len(sections)):
+			if len(sections)-1<x:
+				break
+			if sections[x] == '..':
+				if len(sections)>2 or (len(sections)>1 and os.path.splitdrive(self.path)[0]==''):
+					del sections[x-1]
+					del sections[x-1]
+				else:
+					del sections[x]
+			
+		print "join",sections,os.sep.join(sections)
+		self.subdir = None
+		for s in range(len(sections)):
+			for c in Directory.compfiles.keys():
+				if sections[s][-(len(c)+1):] == '.'+c and self.isfile(os.sep+os.sep.join(sections[:s+1])):
+					self.subdir = Directory.compfiles[c](self,os.sep+os.sep.join(sections[:s+1]))
+					if not self.subdir.chdir(os.sep.join(sections[s+1:])):
+						return
+					print "ZipDir",os.sep.join(sections[:s+1]),os.sep.join(sections[s+1:])
+					#sections = sections[:s+1]
+					break
+
+		self.path = os.sep+os.sep.join(sections)+os.sep
+		self.dir = None
+	
+	def open(self,filename):
+		if self.subdir!=None:
+			return self.subdir.open(filename)
+		
+		return file(os.path.join(self.path,filename),'rb')
+						
---
+++ bin/view.py
@@ -0,0 +1,212 @@
+#!/usr/bin/python
+
+import pygame,os
+from pygame.locals import *
+from directory import Directory
+from sys import argv
+from os.path import dirname,join
+
+class Viewer:
+	def __init__(self,dir):
+		self.dir = dir
+		pygame.init()
+		modes = pygame.display.list_modes()
+		self.screen = pygame.display.set_mode(modes[0],pygame.FULLSCREEN)
+		self.loc = Directory(dir)
+		print argv
+		self.myfont = pygame.font.Font(join(dirname(argv[0]),"verdana.ttf"),32)
+		self.index = 0
+		self.list = [".."]+self.loc.listdir()
+		self.collen = int((self.screen.get_size()[1]*1.0)/self.myfont.size(self.list[1])[1])
+		print "collen",self.collen,self.myfont.size(self.list[1])[1],self.screen.get_size()[1]
+		self.draw_list()
+		clock = pygame.time.Clock()
+		while 1:
+			clock.tick(5)
+			for event in pygame.event.get():
+				if event.type is QUIT:
+					return
+				elif event.type is KEYDOWN:
+					oldindex = self.index
+					olddir = self.loc.path
+					if event.key == K_ESCAPE:
+						return
+					elif event.key == K_DOWN or event.key == K_UP or event.key == K_LEFT or event.key == K_RIGHT:
+						if event.key == K_DOWN or event.key == K_RIGHT:
+							if event.key == K_DOWN:
+								self.index +=1
+							else: # K_RIGHT
+								self.index += self.collen
+								if self.index>=len(self.list):
+									while self.index>=self.collen:
+										self.index -= self.collen
+							if self.index>=len(self.list):
+								self.index -= len(self.list)
+						else: # K_UP or K_LEFT
+							if event.key == K_UP:
+								self.index = self.index - 1
+							else:
+								self.index = self.index - self.collen
+							if self.index<0:
+								self.index = len(self.list)+self.index
+
+					elif event.key == K_RETURN or event.key == K_BACKSPACE:
+						if event.key == K_BACKSPACE:
+							self.index = 0
+							
+						if self.loc.isfile(self.list[self.index]):
+							if self.list[self.index][-4:]=='.cbz' or self.list[self.index][-4:]=='.zip' or self.list[self.index][-4:]=='.cbr' or self.list[self.index][-4:] == ".rar":
+								if self.list[self.index]!="..":
+									self.loc.chdir(self.list[self.index])
+									self.list = [".."]+self.loc.listdir()
+									self.index = 0
+								else:
+									place = self.loc.path[-1]
+									self.loc.chdir("..")
+									self.list = [".."]+self.loc.listdir()
+									self.index=0
+
+									for l in range(len(self.list)):
+										if self.list[l]==place:
+											self.index = l
+											break
+							else:
+								self.index -=1
+								self.browser()
+								self.index +=1
+								self.draw_list()
+						else:
+							if self.list[self.index]!="..":
+								self.loc.chdir(self.list[self.index])
+								self.list = [".."]+self.loc.listdir()
+								self.index = 0
+							else:
+								place = [x for x in self.loc.path.split(os.sep) if x!=""][-1]
+								print "dir",self.loc.path#,Directory.sep.findall(self.loc.path)
+								self.loc.chdir("..")
+								self.list = [".."]+self.loc.listdir()
+								self.index=0
+								print "looking for",place
+								for l in range(len(self.list)):
+									print self.list[l]
+									if self.list[l]==place:
+										self.index = l
+										break
+					elif event.key == K_END:
+						self.index = len(self.list)-1
+					elif event.key == K_HOME:
+						self.index = 0
+					else:
+						print "No binding for "+str(pygame.key.name(event.key))
+					if oldindex!=self.index or self.loc.path!=olddir:
+						self.draw_list()
+					
+
+	def draw_list(self):
+		self.screen.fill((0,0,0))
+		try:
+			height = self.myfont.size(self.list[1])[1]
+		except IndexError:
+			height=20
+		except:
+			print "list",self.list
+			raise
+		y = 0
+		min_x = 20
+		x = min_x
+		max_x = 0
+		for f in self.list:
+			if self.myfont.size(f)[0]>max_x:
+				max_x = self.myfont.size(f)[0]
+				
+		firstdraw = 0
+		while self.index-firstdraw>self.collen*int(self.screen.get_size()[0]/(max_x*1.0)):
+			if firstdraw+self.collen>self.index:
+				break
+			firstdraw += self.collen
+
+		for i in range(firstdraw, len(self.list)):
+			f = self.list[i]
+			if not self.loc.isfile(f):
+				f = "["+f+"]"
+			if y == self.collen:
+				y = 0
+				x += max_x+min_x
+			if self.index == i:
+				self.screen.blit(self.myfont.render(f,True,(255,255,255),(255,0,0)),(x,y*height))
+			else:
+				self.screen.blit(self.myfont.render(f,True,(255,255,255)),(x,y*height))
+
+			y += 1
+		pygame.display.flip()		
+
+	def browser(self):
+		image = self.get_image(self.loc.listdir()[self.index])
+		pygame.display.flip()
+		while 1:
+			for event in pygame.event.get():
+				if event.type is QUIT:
+					return
+				elif event.type is KEYDOWN:
+					if event.key == K_ESCAPE or event.key == K_BACKSPACE:
+						return
+					elif event.key == K_PAGEDOWN or event.key == K_PAGEUP:
+						if event.key == K_PAGEDOWN:
+							self.index = self.index + 1
+							if self.index>len(self.loc.listdir())-1:
+								self.index = len(self.loc.listdir())-1							
+						else:
+							self.index = self.index - 1
+							if self.index<0:
+								self.index = 0
+							
+						
+						image = self.get_image(self.loc.listdir()[self.index])
+						pygame.display.flip()
+					else:
+						print "No binding for "+str(pygame.key.name(event.key))
+
+	def get_image(self,filename):
+		try:
+			print filename
+			f = self.loc.open(filename)
+			print f
+			image = pygame.image.load(f,filename).convert()
+			r=[0,0]
+			for i in range(2):
+				r[i] = self.screen.get_size()[i]/(image.get_size()[i]*1.0)
+			if r[0]>r[1]:
+				ratio = r[1]
+			else:
+				ratio = r[0]
+			scr = (self.screen.get_size()[0]/(self.screen.get_size()[1]*1.0))
+			imr = (image.get_size()[0]/(image.get_size()[1]*1.0))
+			print "imr",scr,imr
+			if imr<1.0:
+				print "small ratio",ratio
+				image = pygame.transform.rotate(image,90)
+				for i in range(2):
+					r[i] = self.screen.get_size()[i]/(image.get_size()[i]*1.0)
+				if r[0]>r[1]:
+					ratio = r[1]
+				else:
+					ratio = r[0]
+
+			print ratio,scr,image.get_size(),(image.get_size()[0]*ratio,image.get_size()[1]*ratio),self.screen.get_size()
+			image = pygame.transform.scale(image,(image.get_size()[0]*ratio,image.get_size()[1]*ratio))
+			self.screen.fill((0,0,0))
+			self.screen.blit(image, ((self.screen.get_size()[0]-image.get_size()[0])/2, (self.screen.get_size()[1]-image.get_size()[1])/2))
+			return image
+		except pygame.error, message:
+			if str(message) == "Unsupported image format":
+				return None
+			else:
+				print "\""+str(message)+"\""
+				raise
+
+try:
+	#Viewer(r"f:\\temp\\flirts")
+	Viewer(os.getcwd())
+except:
+	print "err"
+	raise
