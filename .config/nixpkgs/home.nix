17a2660763f0213e7d9547badfc1e8fe77679c8b
Tom Parker-Shemilt <tom.parkershemilt@doxy.me>
2023-03-05 19:03:00 +0000

    Add pulseaudio-control

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -13,6 +13,25 @@
         withPyInotify = true;
         withMusicBrainzNgs = true;
       };
+      polybar-pulseaudio-control = stdenv.mkDerivation rec {
+        pname = "polybar-pulseaudio-control";
+        version = "3.1.1";
+        src = fetchFromGitHub {
+          owner = "marioortizmanero";
+          repo = "polybar-pulseaudio-control";
+          rev = "v3.1.1";
+          sha256 = "sha256-egCBCnhnmHHKFeDkpaF9Upv/oZ0K3XGyutnp4slq9Vc=";
+        };
+
+        buildInputs = [
+          pulseaudio
+        ];
+
+        installPhase = ''
+          mkdir -p $out/bin
+          cp pulseaudio-control.bash $out/bin/pulseaudio-control
+        '';
+      };
       my-python-packages = p: with p; [
         pip
       ];
@@ -22,7 +41,6 @@
       pavucontrol
       rustup
       feh
-      albert
       udiskie
       redshift
       rofi
@@ -33,6 +51,7 @@
       (pkgs.python3.withPackages my-python-packages)
       pkgsUnstable.zoom-us
       dunst
+      polybar-pulseaudio-control
     ];
   };
 
